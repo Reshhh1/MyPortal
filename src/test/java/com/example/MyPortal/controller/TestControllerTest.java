e1e21cc8aa3bd600f9997b2a7cdc4d66d48a5554
Reshwan Barhoe <2054475@talnet.nl>
2025-04-15 10:57:28 +0200

    Added controller test

---
+++ src/test/java/com/example/MyPortal/controller/TestControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.MyPortal.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testEndPoint() throws Exception {
+        mockMvc.perform(
+                post("/testing-path")
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Successfully unauthorized requested")));
+    }
+}
